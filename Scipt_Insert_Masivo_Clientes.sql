0dab812ee2db82629f57ea27c6c8fd05e6b6148e
Rolando Varela <78666529+RMVARELAA@users.noreply.github.com>
2025-11-30 00:03:24 -0600

    Script to insert data
    Script to insert data into the Table Clientes

---
+++ Scipt_Insert_Masivo_Clientes.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.Clientes 
+(Nombre, Identidad, FechaNacimiento, IdTipoCliente, Telefono, Email, Direccion, UsuarioCreacion)
+VALUES
+('Juan Pérez',          '0801199000011', '1990-01-10', 1, '9999-1111', 'juan.perez@mail.com', 'Tegucigalpa', 'system'),
+('María Gómez',         '0801199500055', '1995-05-15', 1, '8888-2222', 'maria.gomez@mail.com', 'San Pedro Sula', 'system'),
+('Empresa XYZ S.A.',    '0601198500999', '1985-11-20', 2, '2266-1122', 'contacto@xyz.com', 'Choloma', 'system'),
+('Servicios ABC LLC',   '0501198000666', '1980-08-08', 2, '2233-4455', 'info@abc.com', 'El Progreso', 'system');
